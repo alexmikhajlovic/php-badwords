e5629bd474c6e1b15713c204763ad7f1944526d1
Alex <alexmikhajlovic@gmail.com>
2021-02-24 17:23:21 +0100

    Init

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    $text = 'PHP is a popular general-purpose scripting language that is especially suited to web development.';
+    $censored = '***';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+
+    <img src="https://cdn.iconscout.com/icon/free/png-256/php-2038871-1720084.png" alt="">
+
+    <h1><?php echo $text ?></h1>
+    
+</body>
+</html>
